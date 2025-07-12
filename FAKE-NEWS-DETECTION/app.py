051c77aa23b66a8441c9c0e7435391c61479e03d
rushitha1612 <rushitha1612@gmail.com>
2025-07-12 18:49:07 +0530

    Add files via upload

---
+++ FAKE-NEWS-DETECTION/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import joblib
+model = joblib.load("fake_news_model.pkl")
+vectorizer = joblib.load("tfidf_vectorizer.pkl")
+st.title("📰 Fake News Detection App")
+user_input = st.text_area("Enter a news article or headline:")
+if st.button("Check"):
+    if user_input.strip() == "":
+        st.warning("Please enter some text.")
+    else:
+        transformed_input = vectorizer.transform([user_input])
+        prediction = model.predict(transformed_input)
+        result = "🔴 Fake News" if prediction[0] == 0 else "🟢 Real News"
+        st.subheader("Prediction:")
+        st.success(result)
